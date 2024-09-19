41f50200e0a03ad6866fad431a08908248fa108b
evseevegor <evseevegor709icloud@EGOR0A58>
2024-09-19 23:13:09 +0500

    task1#

---
+++ Tyuiu.EvseevEI.Sprint0.Task1.V0/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Tyuiu.EvseevEI.Sprint0.Task1.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            string name = "Egor";
+            int age = 17 ;
+            Console.WriteLine("Привет меня зовут " + name + " мне " + age + " лет");
+            Console.ReadKey();
+        }
+    }
+}
