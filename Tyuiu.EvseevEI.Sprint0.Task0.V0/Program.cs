000408fb9d530920162ed9bced68601989b3da8f
evseevegor709icloud <evseevegor709icloud@EGOR0A58>
2024-09-19 13:24:55 +0500

    Add project files.

---
+++ Tyuiu.EvseevEI.Sprint0.Task0.V0/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Tyuiu.EvseevEI.Sprint0.Task0.V0_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
