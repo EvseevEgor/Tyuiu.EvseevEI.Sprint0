da5aad60a010042506f4522cd8aaa40475b94ee8
evseevegor <evseevegor709icloud@EGOR0A58>
2024-10-02 01:03:12 +0500

    task4

---
+++ Tyuiu.EvseevEI.Sprint0.Task3.V0.Lib/DataService.cs
@@ -0,0 +1,25 @@
+﻿using System.Numerics;
+
+namespace Tyuiu.EvseevEI.Sprint0.Task3.V0.Lib
+{
+    public class DataService
+    {
+        public static int Addition(int a,int b)
+        {
+            return a + b;
+        }
+        public static int Subtraction(int a, int b)
+        {
+            return a - b;
+        }
+        public static int Multiplications(int a, int b)
+        {
+            return a * b;
+        }
+        public static int Division(int a, int b)
+        {
+            return a/ b;
+        }
+
+    }
+}
